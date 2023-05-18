803b3724c4c6f1b41fa2f2c9652e532ca6fa2d87
mezzera <130520323+mezzera@users.noreply.github.com>
2023-05-18 15:46:00 -0300

    Tarea numero 10 Javascript

---
+++ tarean10.js
@@ -0,0 +1,21 @@
+let nombre = "Luciano";
+let apellido = "Ponce";
+let num1 = 5;
+let num2 = 3;
+let num3 = 1994;
+let suma;
+let suma2;
+let suma3;
+
+
+console.log(nombre ,"", apellido);
+
+
+suma = (num1+num2);
+console.log (suma);
+
+suma2 = (suma+num3);
+console.log(suma2);
+
+suma3 = (nombre + num3);
+console.log(suma3);
